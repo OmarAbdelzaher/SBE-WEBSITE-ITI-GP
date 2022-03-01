0ad14a953352087b4fea12c1b22bbed40af429bc
zeinaayman <zeinaayman14@gmail.com>
2022-03-02 00:34:28 +0200

    pull

--- sbe-website-frontend/src/App.js
+++ sbe-website-frontend/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Hello</h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>ALOOOOOOOOOOOOOOOOOOOOO</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
