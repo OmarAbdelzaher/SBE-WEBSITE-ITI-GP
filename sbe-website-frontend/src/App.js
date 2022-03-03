41f029ddd0b4f78ea7a15e31c31d6fff986cf9bd
zeinaayman <zeinaayman14@gmail.com>
2022-03-03 02:54:41 +0200

    pull

--- sbe-website-frontend/src/App.js
+++ sbe-website-frontend/src/App.js
@@ -9,26 +9,6 @@ import Form from './components/Form';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <h1>Hello</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>ALOOOOOOOOOOOOOOOOOOOOO</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-=======
     <>
       <BrowserRouter>
         <Switch>
@@ -39,7 +19,6 @@ function App() {
         </Switch>
       </BrowserRouter>
     </>
->>>>>>> 27eb928ad64821dc42abb652a6a8f225a57841cd
   );
 }
 export default App;
