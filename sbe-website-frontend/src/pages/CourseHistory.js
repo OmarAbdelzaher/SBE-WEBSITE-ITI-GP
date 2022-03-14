533de7b3ee5ff51044a7ba99ee68f72a656be75e
NourhanIbraheem <nouraibraheem82@gmail.com>
2022-03-14 19:50:39 +0200

    edit history page

--- sbe-website-frontend/src/pages/CourseHistory.js
+++ sbe-website-frontend/src/pages/CourseHistory.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesRight, faLink} from "@fortawesome/free-solid-svg-icons";
+
 
 const CourseHistory = () => {
     const [coursehistory, setCoursesHistory] = useState([]);
@@ -23,37 +26,51 @@ const CourseHistory = () => {
     }, []);
     return (
     <>
-      <section className=" h-custom py-5">
+      <section className="h-custom">
         <div className="container ">
           <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-lg-8 col-xl-12 card rounded-3 courses-b border border-2 border-light">
-              <div className="">
+            <div className="col-lg-8 col-xl-10 card rounded-3 courses-b border border-2 border-light">
+              <div>
                 <div className="card-body p-4 p-md-5">
                   <h1 className="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2 bg-light rounded text-center nav-links border border-dark">
                     History of {coureName}
                   </h1>
                 </div>
+                <div className="row ">
+                  <table className="text-light table table-hover fs-4">
+                    <thead>
+                      <tr>
+                        <th>Year</th>
+                        <th>Staff</th>
+                        <th>Material Link</th>
+                      </tr>
+                    </thead>
 
                 {coursehistory.map((item) => {
                   return (
                     <>
-                      <div
-                        className="row justify-content-center align-items-center "
+                      <tbody
+                        className="mb-3 fs-4"
                         key={item.course_id}
                       >
-                        <h2 className="bg-light py-3 nav-links">Year: {item.year}</h2>{" "}
-                        <h3 className="col-12">Dr: {item.staff_id}</h3>
-                          <a href={item.materials}>Materials Link </a>
-                      </div>
+                        <tr className="tr">
+                        <td className='table-b'>{item.year}</td>{" "}
+                        <td className="table-b">{item.staff_id}</td>
+                        <td><a href={item.materials} style={{ color: "#ffff", textDecoration:"none" }}>
+                        <FontAwesomeIcon icon={faLink} /> {" "}
+                        Click To Materials Link </a></td>
+                        </tr>
+                      </tbody>
                     </>
                   );
                 })}
+                 </table>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="margin-b"></div>
-
       </section>
     </>
   );
